1af1d8ba6875b0e7b13f6173fd4609fb1b2660d0
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-12 13:17:06 +0100

    Install aws-sdk, make dhange to GoodCard and GoodList

--- src/components/GoodList/GoodList.jsx
+++ src/components/GoodList/GoodList.jsx
@@ -1,12 +1,46 @@
-import { GoodCard } from '../GoodCard/GoodCard';
+import React, { useState, useEffect } from 'react';
+import { GoodCard } from '../GoodCard';
 import './GoodList.css';
 
-export const GoodList = ({ goods }) => {
+export const GoodList = () => {
+  const [, setGoods] = useState([]); // Removed 'goods' variable
+  const [displayedGoods, setDisplayedGoods] = useState([]);
+
+  useEffect(() => {
+    // Function to retrieve and set 4 cards from the database
+    const fetchAndSetGoods = () => {
+      fetch('https://nyb-project-production.up.railway.app/boats/featured')
+          .then((response) => response.json())
+          .then((data) => {
+            setGoods(data);
+            // Randomly shuffle the data and slice the first 4 cards
+            const shuffledData = shuffleArray(data);
+            setDisplayedGoods(shuffledData.slice(0, 4));
+          })
+          .catch((error) => console.error('Error fetching data:', error));
+    };
+
+    // Function to shuffle an array
+    const shuffleArray = (array) => {
+      const shuffledArray = [...array];
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+      return shuffledArray;
+    };
+
+    // Call the function to fetch and set the initial 4 cards
+    fetchAndSetGoods();
+  }, []);
+
   return (
-    <div className="GoodList">
-      {goods.map((good) => (
-        <GoodCard good={good} key={good.id} />
-      )).slice(0,4)}
-    </div>
+      <div className="GoodList">
+        <div className="card-container">
+          {displayedGoods.map((good) => (
+              <GoodCard key={good.id} good={good} />
+          ))}
+        </div>
+      </div>
   );
 };
